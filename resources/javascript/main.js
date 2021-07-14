eb0632139c6bca2f02d7223c490f296d2ed806ff
Saif Ullah <mrsaifullah52.1@gmail.com>
2021-07-14 20:20:58 +0500

    Uploading to Github

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -7,11 +7,10 @@ class DigitalPet {
     this.foodpoints = [10, 10, -20, 15, -40, -30, 20, 40, 15, 15];
     this.complimentArray = [];
     this.jsonData = [];
+    this.metabolism;
     this.GameScore(60, true);
     this.GamePlayerName();
     this.FetchApi();
-
-    // this.reTime = 3;
 
     // gsap timelines
     this.t1 = gsap.timeline({ repeat: 0, repeatDelay: 0 });
@@ -20,41 +19,39 @@ class DigitalPet {
     this.t4 = gsap.timeline({ repeat: 0, repeatDelay: 0 });
     this.t5 = gsap.timeline({ repeat: 0, repeatDelay: 0 });
     this.t6 = gsap.timeline({ repeat: 0, repeatDelay: 0 });
-    this.talkLip = gsap.timeline({ repeat: 3, repeatDelay: 0 });
-    this.feed = gsap.timeline({ repeat: 0, repeatDelay: 0 })
+    this.talkLip = gsap.timeline({ repeat: 2, repeatDelay: 0 });
+    this.feed = gsap.timeline({ repeat: 0, repeatDelay: 0 });
   }
 
   // Main Functionalities
+  // starting game
   StartGame = () => {
-    console.log("startbtn")
     gsap.timeline()
       .to("#startBtn", { display: "none", scale: 0.0, duration: 2 })
       .from(".getDetails", { display: "block", scale: 0, duration: 2 })
       .to(".getDetails", { display: "block", scale: 1, duration: 2 });
-
+    this.SpeedingDownMetabolism;
   }
+  // restarting game
   RestartGame = () => {
-    console.log("RestartGame");
     this.score = 0;
     this.GameScore(60, true);
     gsap.to("#game_end", { display: "none" })
-    gsap.to("#game", { display: "block" })
+    gsap.to("#crosses", { display: "none" })
     gsap.to("#food_list", { display: "none" });
     gsap.to("#petEmotions", { display: "none" });
     gsap.to("#food", { display: "none" });
     gsap.to("#getcomplimentBtns", { display: "none" });
     gsap.to("#arms_up", { display: "none", });
+    gsap.to("#game", { display: "block" })
     gsap.to("#arms_down", { display: "inline-block", });
-
-    this.metabolism = setInterval(
-      this.SpeedingDownMetabolism, 1000);
-
+    this.metabolism = setInterval(this.SpeedingDownMetabolism, 1000);
   }
+  // game end
   GameEnd = () => {
     gsap.to("#game_end", { display: "block" })
-    gsap.to("#game", { display: "none" })
+    gsap.to("#crosses", { display: "block" })
     clearInterval(this.metabolism);
-    console.log("GameEnd")
   }
   // setting Food Points
   GameScore(gamescore = 60, gameStatus) {
@@ -70,12 +67,11 @@ class DigitalPet {
       this.score = this.score + gamescore;
       document.getElementById("foodPts").innerHTML = `Metabolism Points: ${0} Pts`;
     }
-
   }
   // setting player name
   GamePlayerName(gamePlayerName) {
     if (gamePlayerName != null) {
-      this.username = gamename;
+      this.username = gamePlayerName;
       document.getElementById("pName").innerHTML = `Player Name: ${this.username}`;
     } else {
       document.getElementById("pName").innerHTML = `Player Name: ${this.username}`;
@@ -88,6 +84,7 @@ class DigitalPet {
       .to("#before_starting_game", { display: "none", scale: 0.0, duration: 2 })
       .to("#game", { display: "block", duration: 1, });
     this.GamePlayerName(name);
+    this.metabolism = setInterval(this.SpeedingDownMetabolism, 1000);
   }
 
   // activating compliment section
@@ -104,18 +101,19 @@ class DigitalPet {
 
   // fetching data from json
   FetchApi() {
-    fetch('/resources/javascript/data.json')
+    fetch('resources/javascript/data.json')
       .then((res) => res.json())
       .then((data) => {
         this.jsonData = data;
       })
       .catch(err => console.error(err));
   }
+
   // setting food items
   SetFood() {
-    // this.fooditems = this.jsonData[1];
-    console.log(this.fooditems, "this.fooditems");
+    this.fooditems = this.jsonData[1];
   }
+  // getting Compliments from Pet
   GetComplimentFromPet = (type, count = 2) => {
     // face expressions
     this.Talking(type)
@@ -130,6 +128,7 @@ class DigitalPet {
         j++;
       }
     }
+    // displaying word by word
     let random = Math.floor(Math.random() * this.complimentArray.length);
     let textToDisplay = this.complimentArray[random];
     textToDisplay = textToDisplay.split(" ");
@@ -145,6 +144,7 @@ class DigitalPet {
     gsap.to(output, { display: "inline-block", duration: 2, scale: 1 });
   }
 
+  // what is pet Current Mood
   PetMood() {
     gsap.to("#food_list", { display: "none", scale: 0.0, duration: 2 });
     gsap.to("#petEmotions", { display: "none", scale: 0.0, duration: 2 });
@@ -163,6 +163,7 @@ class DigitalPet {
     this.GetComplimentFromPet(moodNum, 0);
   }
 
+  // Showing Pet Food List
   PetFood = () => {
     gsap.to("#compliments", { display: "none", scale: 0.0, duration: 2 });
     gsap.to("#getcomplimentBtns", { display: "none", scale: 0.0, duration: 2 });
@@ -174,23 +175,21 @@ class DigitalPet {
     gsap.to("#food_list", { display: "inline-block", duration: 2, scale: 0.8 });
   }
 
+  // Feeding Pet
   FeedingPet(food) {
     if (this.score <= 0) {
       this.GameEnd();
     } else {
       if (this.foodpoints[food] < 0) {
         this.Eating(food, "sad");
-        // this.MakePetFaceSad();
-
         this.GameScore(this.foodpoints[food], true);
       } else {
         this.Eating(food, "happy");
-
-        // this.MakePetFaceHappy();
         this.GameScore(this.foodpoints[food], true);
       }
     }
   }
+  // Let him Eat Random Food
   EatRandomFood = () => {
     gsap.to("#food_list", { display: "none", scale: 0.0, duration: 2 });
     gsap.to("#compliments", { display: "none", scale: 0.0, duration: 2 });
@@ -204,21 +203,17 @@ class DigitalPet {
       this.GameEnd();
     } else {
       if (this.foodpoints[food] < 0) {
-        // this.MakePetFaceSad();
         this.GameScore(this.foodpoints[food], true);
         this.Eating(food, "sad");
       } else {
-        // this.MakePetFaceHappy();
         this.GameScore(this.foodpoints[food], true);
         this.Eating(food, "happy");
       }
     }
   }
 
+  // Eating Food with Face animations
   Eating(food, face) {
-
-    console.log("EAting", food, " in ", face, " face");
-
     this.t1.pause();
     this.t2.pause();
     this.t3.pause();
@@ -226,7 +221,6 @@ class DigitalPet {
     this.t5.pause();
     this.t6.pause();
 
-    // this.feed.delay(5);
     this.feed.clear();
     this.feed.restart();
     this.feed.to("#angry", { duration: 0, display: "none" })
@@ -235,6 +229,8 @@ class DigitalPet {
       .to("#jokey", { duration: 0, display: "none" })
       .to("#neutral", { duration: 0, display: "none" })
       .to("#talking", { duration: 0, display: "none" })
+      .to("#arms_down", { display: "none", })
+      .to("#arms_up", { display: "inline-block", });
 
     if (face == "happy") {
       this.feed.restart();
@@ -252,24 +248,24 @@ class DigitalPet {
 
     }
 
+    // throwing the results on screen
     const output = document.querySelector("#food img");
     const t1 = gsap.timeline();
     const t2 = gsap.timeline({ repeat: 0 });
-    output.src = `/resources/imgs/petfood/${this.fooditems[food]}.png`;
+    output.src = `resources/imgs/petfood/${this.fooditems[food]}.png`;
     t1.from(output, { display: "inline-block", duration: 2, scale: 0.8 });
     t1.to(output, { display: "inline-block", duration: 2, scale: 1 });
-
     t1.eventCallback("onComplete", () => {
       gsap.to("#arms_down", { display: "inline-block", });
       gsap.to("#arms_up", { display: "none", });
 
       t1.pause();
       t2.restart();
-      t2.to(output, { display: "none", duration: 0 })
-
+      t2.to(output, { display: "none", duration: 0 });
     })
   }
 
+  // starting pet Emotions
   PetEmotions() {
     gsap.to("#compliments", { display: "none", scale: 0.0, duration: 2 });
     gsap.to("#food", { display: "none", scale: 0.0, duration: 2 });
@@ -282,24 +278,9 @@ class DigitalPet {
     gsap.to("#petEmotions", { display: "inline-block", duration: 2, scale: 0.8 });
   }
 
-
-  SpeedingUpMetabolism = () => {
-    console.log("SpeedingUp");
-  }
-
+  // metabolism
   SpeedingDownMetabolism = () => {
-
     this.GameScore(-2, true)
-
-    console.log("SpeedingDown");
-  }
-
-  // Animation Functions
-  StartingMouthMovement = () => {
-    console.log("StartingMouthMovement");
-  }
-  StopingMouthMovement = () => {
-    console.log("StopingMouthMovement");
   }
 
   // Face Animations
@@ -344,7 +325,6 @@ class DigitalPet {
       .from("#angry", { duration: 1.5, display: "inline-block", scale: 0.8 })
       .to("#angry", { duration: 1.5, display: "inline-block", scale: 0.8 });
 
-
     if (type == "talking") {
       this.Talking(type);
     } else {
@@ -368,8 +348,6 @@ class DigitalPet {
       .to("#lips", { duration: 0, display: "inline-block" })
       .from("#sad", { duration: 1.5, display: "inline-block", scale: 0.8 })
       .to("#sad", { duration: 1.5, display: "inline-block", scale: 0.8 });
-    // .wait("+=1");
-    // this.t3.eventCallback("onComplete", this.EndFaceAnimation);
 
     if (type == "talking") {
       this.Talking(type);
@@ -401,9 +379,7 @@ class DigitalPet {
       this.t4.eventCallback("onComplete", this.MakePetFaceNeutral);
     }
   }
-
   MakePetFaceNeutral = () => {
-
     this.t1.pause();
     this.t2.pause();
     this.t3.pause();
@@ -421,8 +397,7 @@ class DigitalPet {
 
       .from("#neutral", { duration: 1.5, display: "inline-block", scale: 0.8 })
       .to("#neutral", { duration: 1.5, display: "inline-block", scale: 0.9 });
-    this.t5.eventCallback("onComplete", setInterval(this.MakePetEyesAnimated, 3000))
-
+    this.t5.eventCallback("onComplete", setInterval(this.MakePetEyesAnimated, 3000));
   }
   MakePetEyesAnimated() {
     const eyeballs = document.querySelectorAll("#eyeballs");
@@ -436,9 +411,6 @@ class DigitalPet {
   }
 
   Talking = (type) => {
-
-    console.log("TTalking", type);
-
     this.t1.pause();
     this.t2.pause();
     this.t3.pause();
@@ -482,42 +454,16 @@ class DigitalPet {
         .to(".lips", { display: "none", duration: 0 });
       this.t6.eventCallback("onComplete", this.TalkLipsing);
     }
-
   }
 
   TalkLipsing() {
-    // this.reTime = 3;
-
-    // const tl = gsap.timeline({ repeat: this.reTime, repeatDelay: 0 });
-    this.talkLip.from(".talking_lips2", { duration: 0.6, display: "inline-block", transform: "scale(1,4)" })
+    const tl = gsap.timeline({ repeat: 3, repeatDelay: 0 });
+    tl.clear();
+    tl.restart();
+    tl.from(".talking_lips2", { duration: 0.6, display: "inline-block", transform: "scale(1,4)" })
       .to(".talking_lips2", { duration: 0.6, display: "inline-block", transform: "scale(1,1)" });
-    this.talkLip.eventCallback("onComplete", this.MakePetFaceNeutral);
-    console.log("call back called");
+    tl.eventCallback("onComplete", this.MakePetFaceNeutral);
   }
-
-  // Health Points Management
-  FoodPoisning = (FoodPoisning = false) => {
-    console.log("Food Poisning");
-
-    if (FoodPoisning == true) {
-      this.LostHealthPoints();
-    } else {
-      this.GainedHealthPoints();
-    }
-  }
-
-  GainedHealthPoints = () => {
-    console.log("GainedHealthPoints")
-  }
-  LostHealthPoints = () => {
-    console.log("LostHealthPoints")
-  }
-
-  // Behaviours Functions
 }
-
-
+// Creating DigitalPet Object
 let myPet = new DigitalPet();
-
-
-// gsap.to("h1", {duration: 1, x: 100, y: 100, scale: 0.5, rotation: 180, skewX: 45});
